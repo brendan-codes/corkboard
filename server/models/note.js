43b9ed0693eddef25814530df81a72a720d0efdf
Brendan Stanton <bmsenate@gmail.com>
2016-09-24 12:22:13 -0700

    added schema

--- server/models/note.js
+++ server/models/note.js
@@ -7,7 +7,13 @@ var NoteSchema = new mongoose.Schema({
 				address: String,
 				long: Number,
 				lat: Number
-			   }
+			   },
+	note: String,
+	contact: String,
+	notes: [{message: String, contact: String, created_at: String}],
+	timeout: Date,
+	created_at: Date,
+	updated_at: Date
 });
 
 mongoose.model('Note', NoteSchema);
