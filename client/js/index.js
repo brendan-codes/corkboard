e3dd1c941404b1dcddee8e6f891a4983f6581cc0
Anna Propas <apropas@gmail.com>
2016-09-24 22:13:47 -0700

    fine tuned index style

--- client/js/index.js
+++ client/js/index.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $('#scroll_down').click(function(){
     window.scrollTo(0, document.body.scrollHeight);
   });
+  $('#scroll_up').click(function(){
+    window.scrollTo(0, 0);
+  });
 });
